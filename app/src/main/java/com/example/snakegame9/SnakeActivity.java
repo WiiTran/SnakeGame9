3a6ac19b274837f9536666dcfdbaf106f6fbd231
IronicIronPCAccount <nicholas.kaika.sanchez@gmail.com>
2024-03-24 11:00:11 -0700

    Name Text is now visible

--- app/src/main/java/com/example/snakegame9/SnakeActivity.java
+++ app/src/main/java/com/example/snakegame9/SnakeActivity.java
@@ -1,12 +1,17 @@
 package com.example.snakegame9;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class SnakeActivity extends Activity {
 
@@ -49,6 +54,24 @@ public class SnakeActivity extends Activity {
         );
         params.setMargins(20, 20, 20, 20); // Adjust margins as needed
         layout.addView(pauseButton, params);
+
+        //setContentView(R.layout.activity_snake);
+        //final TextView helloTextView = (TextView) findViewById(R.id.ourNames);
+        //helloTextView.setText("Huy Tran, Nicholas Sanchez");
+
+        TextView textView = new TextView(this);
+        textView.setText("Huy Tran and Nicholas Sanchez");
+        textView.setTextColor(Color.WHITE);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+
+        FrameLayout.LayoutParams textViewParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        textViewParams.gravity = Gravity.TOP | Gravity.END;
+        textViewParams.topMargin = 20;
+        textViewParams.rightMargin = 20;
+        layout.addView(textView, textViewParams);
 
         // Set the FrameLayout as the content view
         setContentView(layout);
